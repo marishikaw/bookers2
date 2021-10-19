580eb98ba3bfdfbb9c918d40063e7710048be942
marishikaw <90078966+marishikaw@users.noreply.github.com>
2021-10-19 06:59:24 +0000

    [Update] user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+  
+  # 複数形
   has_many :books, dependent: :destroy
+  
+  # "_id"は付けない
   attachment :profile_image
   validates :name, presence: true, uniqueness: true, length: { in: 2..20 }
   validates :introduction, length: { maximum: 50 }
